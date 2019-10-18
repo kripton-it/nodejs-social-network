386f723702aebd19d513116b199cf2184aba57de
Mikhail Shardin <kriptonit1988@gmail.com>
2019-10-18 23:58:06 +0500

    Views

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = require("./router");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use('/', router);
+
+app.listen(3000);
