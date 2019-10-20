a42f1793636a48349afc3c779d8770d51d3ed8f4
Mikhail Shardin <kriptonit1988@gmail.com>
2019-10-20 13:32:12 +0500

    Models. Очистка данных перед валидацией

--- models/User.js
+++ models/User.js
@@ -5,37 +5,64 @@ const User = function(data) {
   this.errors = [];
 };
 
+User.prototype.cleanup = function() {
+  if (typeof this.data.username !== "string") {
+    this.data.username = "";
+  }
+  if (typeof this.data.email !== "string") {
+    this.data.email = "";
+  }
+  if (typeof this.data.password !== "string") {
+    this.data.password = "";
+  }
+
+  const { username, email, password } = this.data;
+
+  // get rid of any extra properties
+  this.data = {
+    username: username.trim().toLowerCase(),
+    email: email.trim().toLowerCase(),
+    password
+  };
+};
+
 User.prototype.validate = function() {
-  if (this.data.username === "") {
+  const { username, email, password } = this.data;
+
+  if (username === "") {
     this.errors.push("You must provide a username");
   }
-  if (this.data.username.length > 0 && this.data.username.length < 3) {
+  if (username.length > 0 && username.length < 3) {
     this.errors.push("Your username must have at least 3 characters");
   }
-  if (this.data.username.length > 30) {
+  if (username.length > 30) {
     this.errors.push("Your username cannot exceed 30 characters");
   }
-  if (this.data.username.length > 0 && !validator.isAlphanumeric(this.data.email)) {
+  if (
+    username.length > 0 &&
+    !validator.isAlphanumeric(username)
+  ) {
     this.errors.push("Your username can only contain letters and numbers");
   }
 
-  if (!validator.isEmail(this.data.email)) {
+  if (!validator.isEmail(email)) {
     this.errors.push("You must provide a valid email");
   }
 
-  if (this.data.password === "") {
+  if (password === "") {
     this.errors.push("You must provide a password");
   }
-  if (this.data.password.length > 0 && this.data.password.length < 12) {
+  if (password.length > 0 && password.length < 12) {
     this.errors.push("Your password must have at least 12 characters");
   }
-  if (this.data.password.length > 100) {
+  if (password.length > 100) {
     this.errors.push("Your password cannot exceed 100 characters");
   }
 };
 
 User.prototype.register = function() {
   // Step 1: Validate user data
+  this.cleanup();
   this.validate();
   // Step 2: Only if step 1 is correct,
   //         save the data into a database
